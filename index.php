2ffdb6b1f6d7414120423b139e773efc97b77a3b
Inaldo Monteiro <inaldo.monteiro@inbec.com.br>
2020-05-25 16:04:47 -0300

    Subindo a aplicação

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>agora vai</p>
+
+<?php
+
+echo "Programar todos os dias até o fim da minha vida";
+
+?>
+</body>
+</html>
